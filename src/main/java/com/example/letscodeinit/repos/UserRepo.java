d2b2e4a53008a2ba848a6e19180685d8079422b6
Cold <38143140+Nick302@users.noreply.github.com>
2022-03-05 21:47:54 +0100

    security works v0.2

---
+++ src/main/java/com/example/letscodeinit/repos/UserRepo.java
@@ -0,0 +1,8 @@
+package com.example.letscodeinit.repos;
+
+import com.example.letscodeinit.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User,Long> {
+    User findByUsername(String username); //поиск по юзернейму
+}
